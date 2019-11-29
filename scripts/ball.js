e4080552a40d1f87c50b79580da9752788bcec75
adnan <adnanalfonso.mula@pccomponentes.com>
2019-11-29 14:22:40 +0100

    moved ball creation logic to js

---
+++ scripts/ball.js
@@ -0,0 +1,30 @@
+
+class BallInstance extends ISpriteInstance
+{
+	constructor(runtime)
+	{
+		super();
+	}
+	
+	static create(runtime, x, y, size)
+	{
+		let instance = runtime.objects.ball.createInstance("Main", x, y);
+	
+		let diameter = 40;
+		
+		switch(size) {
+			case 0:
+				diameter = 20;
+			break;
+			case 1:
+				diameter = 40;
+				break;
+			case 2:
+				diameter = 60;
+				break;
+		}
+		
+		instance.width = diameter;
+		instance.height = diameter;
+	}
+}
---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+
+runOnStartup(async runtime =>
+{
+	runtime.objects.ball.setInstanceClass(BallInstance);
+	
+	runtime.addEventListener("beforeprojectstart", () => OnBeforeProjectStart(runtime));
+});
+
+function OnBeforeProjectStart(runtime)
+{
+	//runtime.addEventListener("tick", () => Tick(runtime));	
+}
