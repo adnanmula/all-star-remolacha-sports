07241b1632f62b8c4070a9aef55615f4f4733745
adnan <adnan97m@gmail.com>
2021-12-13 22:34:18 +0100

    fix undetected goal colisions

--- scripts/main.js
+++ scripts/main.js
@@ -3,8 +3,6 @@ import BallInstance from "./ball.js";
 runOnStartup(async runtime =>
 {
 	runtime.objects.ball.setInstanceClass(BallInstance);
-	
-	//runtime.addEventListener("beforeprojectstart", () => OnBeforeProjectStart(runtime));
 });
 
 function OnBeforeProjectStart(runtime)
